1a2bf4f1b44847fa4cee8948277dee2af0c8686b
Arun Sundaram <arunsun@gmail.com>
2011-07-20 22:54:54 -0400

    Now Implemented Feature1, hurray!

---
+++ Feature1/Feature1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VectorModelIRS_2008.Feature1
+{
+    class Feature1 : Interface1
+    {
+        public int GetResult(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Feature1/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VectorModelIRS_2008.Feature1
+{
+    interface Interface1
+    {
+        int GetResult(int a, int b);
+    }
+}
