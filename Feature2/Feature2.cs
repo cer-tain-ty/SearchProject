a0c7e15141aa724fe1ee6ce2af5315ac3549b08e
Arun Sundaram <arunsun@gmail.com>
2011-07-21 12:07:28 -0400

    Implemented Feature 2 successfully.

---
+++ Feature2/Feature2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VectorModelIRS_2008.Feature2
+{
+    class Feature2
+    {
+        public void Test()
+        {
+
+        }
+    }
+}
